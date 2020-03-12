2b5ec0d0a208609424d68f383483eedda4a07433
Larry Schultheis <lshults.21.117@gmail.com>
2020-03-12 15:07:29 -0400

    got up to sorting list

--- AStar/AStar/aStar.cpp
+++ AStar/AStar/aStar.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
+#include <list>
 using namespace std;
 
 struct position
 {
 	int row;
 	int col;
-	int parentRow;
-	int parentCol;
+	position* parent;
 	int gVal;
 	int hVal;
+	int fval;
 };
 
 struct obstacle
@@ -19,18 +21,23 @@ struct obstacle
 };
 
 bool findObstacle(int row, int col, vector<obstacle> obsPos, int obstacles, int direction);
-int findHval(position currentNode, position goalNode);
-int findGval();
+int getDistance(position currentNode, position goalNode);
+bool evaluateSuccess(position currentNode, position goalNode);
+bool checkBounds(position node);
+bool skipSuccessor(list<position> tempList, position successor);
+bool compareValue(position p1, position p2);
 
+int rows, cols;
 
 int main()
 {
 
 	// add your code here
-	int rows, cols, startRow, startCol, goalRow, goalCol, obstacles, tempRow, tempCol, curRow, curCol;
+	int startRow, startCol, goalRow, goalCol, obstacles, tempRow, tempCol, curRow, curCol;
 	bool foundGoal = false;
 	bool done = false;
 	bool obstacleFound = false;
+	bool failure = false;
 
 	cin >> rows;
 	cin >> cols;
@@ -46,8 +53,8 @@ int main()
 
 	vector<int> obstacleRows;
 	vector<int> obstacleCols;
-	vector<position> positionHistory;
-	vector<position> openList;
+	list<position> closedList;
+	list<position> openList;
 	vector<obstacle> obsPos;
 
 	for (int i = 0; i < obstacles; i++) {
@@ -65,11 +72,18 @@ int main()
 
 	startNode.row = curRow;
 	startNode.col = curCol;
+	startNode.hVal = getDistance(startNode, goalNode);
+	startNode.gVal = 0;
+	startNode.parent = NULL;
+	startNode.fval = startNode.hVal + startNode.gVal;
 
-	positionHistory.push_back(startNode);
+	openList.push_back(startNode);
+	cout << startNode.row << " " << startNode.col << " " << endl;
 
 
-	while (!done) {
+
+
+	while (true) {
 
 		////up
 		//if ((curRow > goalRow) && !findObstacle(curRow, curCol, obsPos, obstacles, 0)) {
@@ -101,18 +115,81 @@ int main()
 
 		//}
 
-		position pos;
-		pos.row = curRow;
-		pos.col = curCol;
-		pos.parentRow = curRow;
-		pos.parentCol = curCol;
+		if (openList.size() == 0)
+		{
+			//failure
+			failure = true;
+			break;
+		}
+
+		position chosenNode = openList.front();
+		openList.pop_front();
+
+		if (evaluateSuccess(chosenNode, goalNode))
+		{
+			foundGoal = true;
+			break;
+		}
+
+		closedList.push_back(chosenNode);
+
+		for (int i = 0; i < 4; i++)
+		{
+			position temp;
+			list<position> tempOpenList = openList;
+			list<position> tempClosedList = closedList;
+
+			switch (i)
+			{
+				case 0:
+					temp.row = chosenNode.row + 1;
+					temp.col = chosenNode.col + 1;
+					break;
+				case 1:
+					temp.row = chosenNode.row + 1;
+					temp.col = chosenNode.col - 1;
+					break;
+				case 2:
+					temp.row = chosenNode.row - 1;
+					temp.col = chosenNode.col - 1;
+					break;
+				case 3: 
+					temp.row = chosenNode.row - 1;
+					temp.col = chosenNode.col + 1;
+					break;
+			}
+
+			if (checkBounds(temp))
+			{
+				temp.parent = &chosenNode;
+				temp.hVal = getDistance(temp, goalNode);
+				temp.gVal = chosenNode.gVal + 1;
+				temp.fval = temp.hVal + temp.gVal;
+			}
+
+			if (skipSuccessor(tempOpenList, temp))
+				break;
+			else if (skipSuccessor(tempClosedList, temp))
+				break;
+			else
+			{
+				//add successor
+				openList.push_back(temp);
+				cout << temp.row << " " << temp.col << " " << endl;
+
+				//sort list
+				//sort(openList.begin(), openList.end(), compareValue);
+			}
+		}
+	
 	}
 
 	cout << -1 << " " << -1 << endl;
 
 
-	for (int i = 0; i < positionHistory.size(); i++) {
-		cout << positionHistory.at(i).row << " " << positionHistory.at(i).col << endl;
+	for (int i = 0; i < closedList.size(); i++) {
+		cout << closedList.front().row << " " << closedList.front().col << endl;
+		closedList.pop_front();
 	}
 
 
@@ -127,7 +204,7 @@ int main()
 
 // Returns the h or g value of the current node
 // Parameters: the current node and the start/goal node for g/h values, respectively
-int findValue(position currentNode, position goalNode)
+int getDistance(position currentNode, position goalNode)
 {
 	int rows = abs(goalNode.row - currentNode.row);
 	int cols = abs(goalNode.col - currentNode.col);
@@ -135,6 +212,38 @@ int findValue(position currentNode, position goalNode)
 	return rows + cols;
 }
 
+bool evaluateSuccess(position currentNode, position goalNode)
+{
+	return (currentNode.row == goalNode.row && currentNode.col == goalNode.col);
+}
+
+bool checkBounds(position node)
+{
+	return (node.row >= 0 && node.row < rows && node.col >= 0 && node.col < cols);
+}
+
+bool skipSuccessor(list<position> tempList, position successor)
+{
+	for (int j = 0; j < tempList.size(); j++)
+	{
+		position tempnode = tempList.front();
+		if (tempnode.row == successor.row && tempnode.col == successor.col)
+		{
+			if (tempnode.hVal <= successor.fval)
+			{
+				//skip successor
+				return true;
+			}
+		}
+		tempList.pop_front();
+	}
+	return false;
+}
+
+bool compareValue(position p1, position p2)
+{
+	return (p1.fval < p2.fval);
+}
 
 bool findObstacle(int row, int col, vector<obstacle> obsPos, int obstacles, int direction)
 {
